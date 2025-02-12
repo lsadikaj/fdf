7fcb66886ca7748f0593cf6bd841ba49abd1eeea
Leotrim Sadikaj <lsadikaj@c2r9s5.42lausanne.ch>
2025-02-12 13:54:44 +0100

    suppression .o

--- srcs/color.c
+++ srcs/color.c
@@ -6,7 +6,7 @@
 /*   By: lsadikaj <lsadikaj@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:30:19 by lsadikaj          #+#    #+#             */
-/*   Updated: 2025/02/12 13:35:19 by lsadikaj         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:54:03 by lsadikaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,13 @@ int	gradient_color(t_point p1, t_point p2, float percentage)
 		percentage = 0.0;
 	if (percentage > 1.0)
 		percentage = 1.0;
-
 	red = (int)((1.0 - percentage) * ((p1.color >> 16) & 0xFF) +
-				percentage * ((p2.color >> 16) & 0xFF));
+			percentage * ((p2.color >> 16) & 0xFF));
 	green = (int)((1.0 - percentage) * ((p1.color >> 8) & 0xFF) +
-				  percentage * ((p2.color >> 8) & 0xFF));
+			percentage * ((p2.color >> 8) & 0xFF));
 	blue = (int)((1.0 - percentage) * (p1.color & 0xFF) +
-				 percentage * (p2.color & 0xFF));
-
+			percentage * (p2.color & 0xFF));
+			
 	return ((red << 16) | (green << 8) | blue);
 }
 
@@ -41,7 +40,7 @@ int	get_color(int x, int y, int z, t_fdf *fdf)
 		{0xFFFFFF, 0x00FF00, 0xFF0000}
 	};
 	int	*selected_palette;
-	
+
 	if (fdf->colors[y][x] != -1)
 		return (fdf->colors[y][x]);
 	selected_palette = palettes[fdf->color_palette];
